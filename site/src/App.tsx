bf9cd24504835d32c7176a23e47a04fa6f969810
Antoine Chaumet <antoine.chaumet@etu.unistra.fr>
2025-12-05 00:41:49 +0100

    ajout transitions

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,13 +1,11 @@
 import { HeroFake } from './components/HeroFake';
 import { RealityCheck } from './components/RealityCheck';
-import { CallToAction } from './components/CallToAction';
 
 function App() {
   return (
     <main className="bg-black min-h-screen w-full">
       <HeroFake />
       <RealityCheck />
-      <CallToAction />
     </main>
   );
 }
